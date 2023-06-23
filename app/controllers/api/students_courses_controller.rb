dcf456c11292e2321c33a751cac14e08111780ed
HarshathSundaram <harshath.sm@rently.com>
2023-06-23 11:56:14 +0530

    completed api for student course controller

--- app/controllers/api/students_courses_controller.rb
+++ app/controllers/api/students_courses_controller.rb
@@ -2,33 +2,53 @@ class Api::StudentsCoursesController < Api::ApiController
     # before_action :authenticate_user! 
     # before_action :is_student? 
     def index
-        @courses = Course.all
-        @student = Student.find_by(id:params[:student_id])
+        courses = Course.all
+        student = Student.find_by(id:params[:student_id])
+        unless student and courses
+            render json:{message:"Error in fetching student and courses details"}, status: :not_found
+        else
+            render json:{"courses":courses,"student":student},status: :ok
+        end
     end
 
     def showcourse
-        @student = Student.find_by(id:params[:student_id])
-        @course = Course.find_by(id:params[:course_id])
+        student = Student.find_by(id:params[:student_id])
+        course = student.courses.find_by(id:params[:course_id])
+        unless student and course
+            render json:{message:"Error in fetching student and course details"}, status: :not_found
+        else
+            render json:{"course":course,"student":student},status: :ok
+        end
     end
 
     def showtopic
-        @student = Student.find_by(id:params[:student_id])
-        @course = Course.find_by(id:params[:course_id])
-        @topic = Topic.find_by(id:params[:topic_id])
+        student = Student.find_by(id:params[:student_id])
+        course = student.courses.find_by(id:params[:course_id])
+        topic = course.topics.find_by(id:params[:topic_id])
+        unless student and course and topic
+            render json:{message:"Error in fetching student, course, and topic details"}, status: :not_found
+        else
+            render json:{"course":course,"student":student,"topic":topic},status: :ok
+        end
     end
 
     def enroll
-        @student = Student.find_by(id:params[:student_id])
-        @course = Course.find_by(id:params[:course_id])
-        @student.courses << @course
-        redirect_to student_path(@student),  alert: "Successfully enrolled"     
+        student = Student.find_by(id:params[:student_id])
+        course = Course.find_by(id:params[:course_id])
+        if student.courses << @course
+            render json:{message:"#{student.user.name} successfully enrolled to a course"},status: :created
+        else
+            render json:{message:"Error in enrolling to a course "},status: :not_modified
     end
 
     def unenroll
-        @student = Student.find_by(id:params[:student_id])
-        @course = Course.find_by(id:params[:course_id])
-        @student.courses.delete(@course)
-        redirect_to student_path(@student), alert: "Sucessfully Unenrolled"
+        student = Student.find_by(id:params[:student_id])
+        course = Course.find_by(id:params[:course_id])
+        if student.courses.delete(@course)
+            render json:{message:"#{student.user.name} successfully unenrolled to a course"},status: :accepted
+        else
+            render json:{message:"Error in unenroll a course"},status: :not_modified
+        end
     end
 
     private
