3fff43e259b79448d6118915044eef7197a9a91b
HarshathSundaram <harshath.sm@rently.com>
2023-06-27 14:12:56 +0530

    Completed Client Credentials

--- app/controllers/api/landing_controller.rb
+++ app/controllers/api/landing_controller.rb
@@ -1,9 +1,14 @@
 class Api::LandingController < Api::ApiController
   def index
-    if current_user.userable_type == "Student"
-      render json:{message: "Welcome to Online Examination Portal #{current_user.name} , #{current_user.userable_type}"}
-    elsif current_user.userable_type == "Instructor"
-      render json:{message: "Welcome to Online Examination Portal #{current_user.name} , #{current_user.userable_type}"}
-    end
-   end 
+    if current_user
+      if current_user.userable_type == "Student"
+        render json:{message: "Welcome to Online Examination Portal #{current_user.name} , #{current_user.userable_type}"}
+      elsif current_user.userable_type == "Instructor"
+        render json:{message: "Welcome to Online Examination Portal #{current_user.name} , #{current_user.userable_type}"}
+      end
+    else
+      render json:{message: "Hello!! Welcome to Online Examination Portal Please Register Yourself to Access the Portal!!!!"}, status: :ok
+    end  
+  end
+  
 end
